71e1222ca15560b51ef2213095f0b451177377d2
funkychicken493 <82616867+funkychicken493@users.noreply.github.com>
2022-06-02 22:21:15 -0500

    Clean up code a little bit and comment

--- MM2DZ/mm2dz.py
+++ MM2DZ/mm2dz.py
@@ -1,19 +1,23 @@
 import os
 from os import listdir
 from os.path import isfile, join
-from tkinter.ttk import Separator
 import yaml
 
 path = f"{os.getcwd()}/mythicmobs"
 files = [f for f in listdir(path) if isfile(join(path, f))]
 
 def translate():
+    
     istr = "[" + script_name + "] "
-    print("Translating " + istr + "...")
+    
+    #Print mob information
+    print(istr.capitalize())
     print(istr + "Type: " + str(l[script_name]["Type"]).lower())
     print(istr + "Display: " + str(l[script_name]["Display"]))
     print(istr + "Health: " + str(l[script_name]["Health"]))
     print(istr + "Damage: " + str(l[script_name]["Damage"]))
+    
+    #Add in the denizen keys
     l[script_name]["type"] = "entity"
     l[script_name]["entity_type"] = l[script_name]["Type"]
     l[script_name]["mechanisms"] = {
@@ -21,6 +25,8 @@ def translate():
         "max_health": ifnulldict(l[script_name], "Health", "20"), 
         "health": ifnulldict(l[script_name], "Health", "20")
     }
+    
+    #Get rid of the old keys
     trydel(l[script_name], "Type")
     trydel(l[script_name], "Display")
     trydel(l[script_name], "Health")
@@ -29,24 +35,31 @@ def translate():
     trydel(l[script_name], "Skills")
     
 def ifnulldict(dict, key, default):
+    #Check if the key exists in the dict, if not, return the default
     if key in dict:
         return dict[key]
     else:
         return default
     
 def trydel(dict, key):
+    #Try to delete the key from the dict, if it doesn't exist, do nothing
     if key in dict:
         del dict[key]
 
 for s in files:
+    
+    #Check if the file already is a dsc file, if so, skip it
     if(s.endswith(".dsc")):
         continue
+    
     with open(f"{path}/{s}") as file:
         l = yaml.load(file, Loader=yaml.FullLoader)
+    
+    #For each script, translate it
     for script_name in l:
         translate()
-    #script_name = list(l.keys())[0]
-    #translate()
+    
+    
     with open(f"{path}/{s}.dsc".replace(".yml", ""), 'w') as yaml_file:
         dump = yaml.dump(l, default_flow_style = False, allow_unicode = True, sort_keys=True, indent=4, Dumper=yaml.Dumper)
         yaml_file.write( dump )
